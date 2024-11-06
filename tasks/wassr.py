83f9426742ac50b7531eaeaf61ccf8e667a946b4
Julien Lamy <lamy@unistra.fr>
2024-11-06 17:58:49 +0100

    Add customization parameters to WASSR

--- tasks/wassr.py
+++ tasks/wassr.py
@@ -8,14 +8,14 @@ import spire
 from . import utils
 
 class WASSR(spire.TaskFactory):
-    def __init__(self, image, wassr):
+    def __init__(self, image, wassr, delta_ppm=0.001, ppm_range=None):
         spire.TaskFactory.__init__(self, str(wassr))
         self.file_dep = [image]
         self.targets = [wassr]
-        self.actions = [(__class__.action, (image, wassr))]
+        self.actions = [(__class__.action, (image, wassr, delta_ppm, ppm_range))]
     
     @staticmethod
-    def action(image_path, wassr_path):
+    def action(image_path, wassr_path, delta_ppm=0.001, ppm_range=None):
         # Get the frequency information from the meta-data
         meta_data_path = re.sub("\.nii(\.gz)?$", ".json", str(image_path))
         ppm = utils.get_ppm(meta_data_path)
@@ -29,8 +29,14 @@ class WASSR(spire.TaskFactory):
         ppm = ppm[order]
         data = data[..., order]
         
+        # Keep the requested data
+        if ppm_range:
+            selector = (ppm >= ppm_range[0]) & (ppm <= ppm_range[1])
+            ppm = ppm[selector]
+            data = data[..., selector]
+        
         # Interpolate the Z-spectrum
-        ppm_fine = numpy.arange(ppm.min(), ppm.max(), 0.001)
+        ppm_fine = numpy.arange(ppm.min(), ppm.max()+delta_ppm, delta_ppm)
         interpolator = scipy.interpolate.CubicSpline(ppm, data, axis=3)
         interpolated = interpolator(ppm_fine)
         
