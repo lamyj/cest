9390b44baa44c0a39b8e94768bd7b8bb3bd046bc
Julien Lamy <lamy@unistra.fr>
2024-11-29 18:13:58 +0100

    Add examples

--- examples/processing.py
+++ examples/processing.py
@@ -5,7 +5,7 @@ import pathlib
 import spire.ants
 import yaml
 
-import tasks
+import cest.tasks
 
 root = pathlib.Path(".")
 
@@ -21,26 +21,31 @@ if "Water" in series:
     derived = root/"derived"/"WASSR"
     derived.mkdir(parents=True, exist_ok=True)
 
-    B0 = tasks.WASSR(images["Water"], meta_data["Water"], derived/"B0.nii.gz")
+    B0 = cest.tasks.WASSR(images["Water"], meta_data["Water"], derived/"B0.nii.gz")
     B0_in_Glutamate = spire.ants.ApplyTransforms(
         B0.targets[0], (images["Glutamate"], 0), [],
         derived/"B0_in_Glutamate.nii.gz")
-    shifted = tasks.ShiftSpectrum(
+    shifted = cest.tasks.ShiftSpectrum(
         images["Glutamate"], meta_data["Glutamate"], B0_in_Glutamate.targets[0],
         derived/"Glutamate_shifted.nii.gz")
-    refined = tasks.Refine(
+    
+    ppms = numpy.linspace(-5, 5, 501)
+    refined = cest.tasks.Refine(
         shifted.targets[0], shifted.file_dep[1], numpy.linspace(-5, 5, 501),
         derived/"Glutamate_refined.nii.gz")
+    reference = cest.tasks.ReferenceMap(
+        refined.targets[0], shifted.file_dep[1], 3,
+        derived/"Glutamate_reference_3ppm.nii.gz")
 
 derived = root/"derived"/"dummy"
 derived.mkdir(parents=True, exist_ok=True)
 
-B0 = tasks.WASSR(
+B0 = cest.tasks.WASSR(
     images["Glutamate"], meta_data["Glutamate"], derived/"B0.nii.gz",
     ppm_range=(-5.1, +5.1))
-shifted = tasks.ShiftSpectrum(
+shifted = cest.tasks.ShiftSpectrum(
     images["Glutamate"], meta_data["Glutamate"], B0.targets[0],
     derived/"Glutamate_shifted.nii.gz")
-refined = tasks.Refine(
+refined = cest.tasks.Refine(
     shifted.targets[0], shifted.file_dep[1], numpy.linspace(-5, 5, 501),
     derived/"Glutamate_refined.nii.gz")
---
+++ examples/z_spectra.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot
+import numpy
+
+import cest
+
+# Experimental conditions: gyromagnetic ratio (rad/s/T), main magnetic field (Hz)
+gamma = 267522187.44
+B0 = 300e6
+
+# CW saturation pulse: amplitude (T), duration (s)
+B1_cw = 6e-6
+tau_cw = 1
+
+# Simulation parameters: time step (s), offset of the RF pulse (PPM), main species
+step = 1e-3 # s
+w_ppm = numpy.linspace(-5, 5, 101)
+species_a = cest.species.water
+
+# Shaped pulse. The RF pulses are normalized: multiply by γ⋅B₁⋅steps to get a
+# saturation equivalent to the CW pulse defined above.
+steps = int(round(tau_cw/step)) # Unitless
+pulse = cest.pulses.gaussian(steps) * gamma*B1_cw * steps
+
+# Compute the Z spectra of three species exchanging with water when applying the
+# shaped pulse at the specified offsets
+z_spectra = {}
+for name in ["glutamate", "creatine", "mobile_amides"]:
+    species_b = getattr(cest.species, name)
+    # WARNING: unrealistic concentration, to better show the effect
+    species_b.M0 *= 20
+    
+    z_spectra[name] = numpy.zeros_like(w_ppm)
+    
+    # NOTE: the frequency offset must be converted to Hz
+    for i, w in enumerate(w_ppm*1e-6 * B0):
+        M = numpy.array([0, 0, species_a.M0, 0, 0, species_b.M0, 1])
+        for w1 in pulse:
+            M = cest.two_pools(species_a, species_b, w, w1, step, B0) @ M
+        z_spectra[name][i] = M[2]
+
+labels = {"glutamate": "Glu", "creatine": "Cr", "mobile_amides": "mAmides"}
+
+figure, plot = matplotlib.pyplot.subplots(layout="constrained")
+for name, z_spectrum in z_spectra.items():
+    plot.plot(w_ppm, z_spectrum, label=labels[name])
+
+plot.set(ylim=0, xlabel=r"$\Delta\omega$ (ppm)", ylabel="$M_{z,a}$ (unitless)")
+plot.legend()
+plot.invert_xaxis()
+
+figure.savefig("z_spectra.png")
